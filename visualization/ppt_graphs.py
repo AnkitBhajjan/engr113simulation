d88b7038e78c9adcf71a11add0b3ee19a518e2d6
Maxwell Costantino <mac864@drexel.edu>
2026-05-21 23:58:10 -0400

    Added snr vs ber graph

--- visualization/ppt_graphs.py
+++ visualization/ppt_graphs.py
@@ -24,6 +24,10 @@ received_powers = [linkBudget(distance_km=d) for d in distance_range_km]
 link_margins = [linkMargin(received_power_dBW=p) for p in received_powers]
 signal_to_noise_ratios = [signalToNoiseRatio(distance_km=d) for d in distance_range_km]
 
+#ber calc
+ebn0_sweep = np.linspace(0, 30)
+ber_vals = [bitErrorRate(eb_n0_db=eb) for eb in ebn0_sweep]
+
 # Formatting
 fontname = "serif"
 
@@ -60,5 +64,14 @@ plt.legend()
 plt.show()
 
 # snr vs ber graph
+plt.figure(figsize=(10, 5))
+plt.plot(ebn0_sweep, ber_vals, label='Bit Error Rate (1024-QAM)', color='crimson')
+plt.yscale('log')
+plt.title('Bit Error Rate vs Normalized SNR (Eb/N0)', fontname=fontname, fontsize=14)
+plt.xlabel('Eb/N0 (dB)', fontname=fontname)
+plt.ylabel('Bit Error Rate (BER)', fontname=fontname)
+plt.grid(True, which="both", linestyle=":")
+plt.legend()
+plt.show()
 
 
